fba1f5bf2623ac515fc578186b5efe12d607c56e
Danny Robinson <drobinson@healthgrades.com>
2017-08-22 20:47:27 -0600

    Completed migrations

--- project/_config.py
+++ project/_config.py
@@ -3,9 +3,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktask.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'supersecret'
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
--- project/db_create.py
+++ project/db_create.py
@@ -1,13 +1,9 @@
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
+db.create_all()
+# db.session.add(Task("Finish this tutorial", date(2017, 9, 22), 10, 1))
+# db.session.add(Task("Finish another tutorial", date(2017, 9, 29), 10, 1))
 
-    c = connection.cursor()
-
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL, status INTEGER NOT NULL)""")
-
-    c.execute('INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish work", "09/25/2017", 10, 1)')
-    c.execute('INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish other work", "09/26/2017", 10, 1)')
+db.session.commit()
